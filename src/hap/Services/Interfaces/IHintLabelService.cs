170715247521c56914d72fef5a7e074c6f963db8
Zachary Sims <zac.sims@gmail.com>
2015-02-26 08:37:25 +1100

    Simplifies the solution by removing the 'engine'

---
+++ src/hap/Services/Interfaces/IHintLabelService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace hap.Services.Interfaces
+{
+    public interface IHintLabelService
+    {
+        /// <summary>
+        /// Generate N labels
+        /// </summary>
+        IList<string> GetHintStrings(int count);
+    }
+}
---
+++ src/hap/Services/Interfaces/IHintProviderService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using hap.Models;
+
+namespace hap.Services.Interfaces
+{
+    /// <summary>
+    /// Provides hints for the entire desktop or a given window handle
+    /// </summary>
+    public interface IHintProviderService
+    {
+        /// <summary>
+        /// Enumerate the available hints for the current foreground window
+        /// </summary>
+        /// <returns>The hint session containing the available hints</returns>
+        HintSession EnumHints();
+
+        /// <summary>
+        /// Enumerate the available hints for the given window
+        /// </summary>
+        /// <param name="hWnd">The window handle of window to enumerate hints in</param>
+        /// <returns>The hint session containing the available hints</returns>
+        HintSession EnumHints(IntPtr hWnd);
+    }
+}
