551a178a7bc912d67422a46086f2ceb8b179301f
adrianhajdukiewicz1 <121614179+adrianhajdukiewicz1@users.noreply.github.com>
2023-02-16 12:19:44 +0100

    Feat/add changing font size (#1)
    * fix: update NET framework version from 4.5 to 4.5.1 to make the compilation work
    
    * feat: add option to change font size in the tray menu
    
    * docs: add info on changing font size to readme;

--- src/HuntAndPeck/Properties/Settings.Designer.cs
+++ src/HuntAndPeck/Properties/Settings.Designer.cs
@@ -9,18 +9,31 @@
 //------------------------------------------------------------------------------
 
 namespace HuntAndPeck.Properties {
-    
-    
+
+
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.1.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
-        
+
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-        
+
         public static Settings Default {
             get {
                 return defaultInstance;
             }
         }
+
+    [global::System.Configuration.UserScopedSettingAttribute()]
+    [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+    [global::System.Configuration.DefaultSettingValueAttribute("14")]
+    public string FontSize {
+        get {
+            return ((string)(this["FontSize"]));
+        }
+        set {
+            this["FontSize"] = value;
+        }
+    }
+
     }
 }
